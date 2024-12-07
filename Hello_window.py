ddefacf45f3d11874e5708112473a7839ba6ff0a
KKivik <ivanov_lab@list.ru>
2024-12-07 18:44:19 +0300

    Add files via upload

--- Hello_window.py
+++ Hello_window.py
@@ -4,15 +4,22 @@ import sys
 
 
 from PyQt6 import uic  # Импортируем uic
-from PyQt6.QtWidgets import QApplication, QMainWindow, QWidget, QFileDialog, QDialog
+from PyQt6.QtWidgets import QApplication, QMainWindow, QWidget, QFileDialog, QDialog, QMessageBox, QDialogButtonBox, QComboBox
 from Crypto import Decoder, No_Flash_Drive, No_Key_On_Flash
 
-class Cryptographer_window(QDialog):
+class Dialog(QDialog):
     def __init__(self):
         super().__init__()
-        uic.loadUi('consts/Main_window.ui', self)  # Загружаем дизайн
+        uic.loadUi('consts/Dialog.ui', self)
+        self.comboBox.addItem('C')
+        self.comboBox.addItem('F')
+        self.comboBox.addItem('G')
+        self.comboBox.addItem('D')
+        self.comboBox.addItem('M')
 
 
-
-
-
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = Dialog()
+    ex.show()
+    sys.exit(app.exec())
